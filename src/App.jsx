50565a13ed9e3c28992157f235ad7d41a41e9f58
k1nnyyY <osman.haciev231@gmail.com>
2024-12-06 13:44:57 +0400

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,77 @@
 import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { GlobalStyles } from "./GlobalStyles";
 import { lightTheme, darkTheme } from "./theme";
 import WelcomePage from "./Pages/WelcomePage";
 import QuizPage from "./Pages/QuizPage";
 import ProfilePage from "./Pages/ProfilePage";
-import { useNavigate } from "react-router-dom";
-import { Navigate } from "react-router-dom";
-
 
 const App = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(null);
-  const navigate = useNavigate();
 
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
   };
 
- 
-const checkUser = async () => {
-  const initData = window.Telegram.WebApp.initData;
+  const checkUser = async () => {
+    const initData = window.Telegram.WebApp.initData;
 
-  if (!initData) {
-    console.error("Telegram InitData is missing");
-    setIsAuthenticated(false);
-    return;
-  }
-
-  try {
-    const response = await fetch("http://localhost:9000/api/auth/verify", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ initData }),
-    });
-
-    const result = await response.json();
-
-    if (result.redirect === "/welcome") {
+    if (!initData) {
+      console.error("Telegram InitData is missing");
       setIsAuthenticated(false);
-      navigate("/welcome");
-    } else if (result.redirect === "/profile") {
-      setIsAuthenticated(true);
-      navigate("/profile");
+      return;
     }
-  } catch (error) {
-    console.error("Error verifying user:", error);
-    setIsAuthenticated(false);
-  }
-};
+
+    try {
+      const response = await fetch("http://localhost:9000/api/auth/verify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ initData }),
+      });
+
+      const result = await response.json();
+
+      if (result.redirect === "/welcome") {
+        setIsAuthenticated(false);
+      } else if (result.redirect === "/profile") {
+        setIsAuthenticated(true);
+      }
+    } catch (error) {
+      console.error("Error verifying user:", error);
+      setIsAuthenticated(false);
+    }
+  };
+
   useEffect(() => {
     checkUser();
   }, []);
 
   if (isAuthenticated === null) {
-    return <div>Loading...</div>; // Отображение загрузки до завершения проверки
+    return <div>Loading...</div>; // Show loading until the check is complete
   }
 
   return (
     <ThemeProvider theme={isDarkMode ? lightTheme : darkTheme}>
       <GlobalStyles />
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              isAuthenticated ? (
-                <Navigate to="/profile" />
-              ) : (
-                <Navigate to="/welcome" />
-              )
-            }
-          />
-          <Route path="/welcome" element={<WelcomePage />} />
-          <Route path="/quiz" element={<QuizPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? (
+              <Navigate to="/profile" />
+            ) : (
+              <Navigate to="/welcome" />
+            )
+          }
+        />
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
       <button
         style={{
           position: "absolute",
